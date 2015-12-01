036f0bf488c26ea6f6dc12280a3cbd50d9cdca43
Yusuke Oshiro <oshiro@jsk.imi.i.u-tokyo.ac.jp>
2015-12-01 17:24:20 +0900

    publish bounding-box regularly

--- jsk_footstep_planner/scripts/footstep_array_to_bounding_box_array.py
+++ jsk_footstep_planner/scripts/footstep_array_to_bounding_box_array.py
@@ -5,6 +5,7 @@ from jsk_footstep_msgs.msg import Footstep, FootstepArray
 from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
 
 def callback(msg):
+    global box_array
     box_array = BoundingBoxArray()
     box_array.header = msg.header
     for footstep in msg.footsteps:
@@ -21,6 +22,18 @@ if __name__ == "__main__":
     rospy.init_node("footstep_array_to_bounding_box")
     z_max = rospy.get_param('~z_max',0.0005)
     z_min = rospy.get_param('~z_min',-0.0005)
+    publish_once = rospy.get_param('~publish_once', True)
+    publish_rate = rospy.get_param('~publish_rate', 30)
     pub = rospy.Publisher("~output", BoundingBoxArray)
     sub = rospy.Subscriber("~input", FootstepArray, callback)
-    rospy.spin()
+    rate = rospy.Rate(publish_rate)
+    if not publish_once :
+        while not rospy.is_shutdown():
+            try :
+                pub.publish(box_array)
+            except NameError :
+                pass
+            finally :
+                rate.sleep()
+    else :
+        rospy.spin()
